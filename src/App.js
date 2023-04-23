e27bd40d4806b2856f337dc383fb04424bacd64d
Akthiha <akthiha@gmail.com>
2023-04-24 00:07:51 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
-function App() {
+function Calculator() {
+  const [calculation, setCalculation] = useState("");
+
+  const handleClick = (e) => {
+    const value = e.target.value;
+    setCalculation(calculation + value);
+  };
+
+  const handleClear = () => {
+    setCalculation("");
+  };
+
+  const handleCalculate = () => {
+    try {
+      const result = eval(calculation);
+      setCalculation(result.toString());
+    } catch (error) {
+      setCalculation("Error");
+    }
+  };
+
+  const handleSquareRoot = () => {
+    setCalculation(`Math.sqrt(${calculation})`);
+  };
+
+  const handleSquared = () => {
+    setCalculation(`Math.pow(${calculation}, 2)`);
+  };
+
+  const handlePower = () => {
+    setCalculation(`Math.pow(${calculation},`);
+  };
+
+  const handleLog = () => {
+    setCalculation(`Math.log10(${calculation})`);
+  };
+
+  const handleNaturalLog = () => {
+    setCalculation(`Math.log(${calculation})`);
+  };
+
+  const handlePi = () => {
+    setCalculation(`${Math.PI}`);
+  };
+
+  const handleE = () => {
+    setCalculation(`${Math.E}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <input className="input" type="text" value={calculation} disabled />
+      <div className="buttons">
+        <button onClick={handleClear} className="button clear">
+          AC
+        </button>
+        <button onClick={handleClick} className="button" value="(">
+          (
+        </button>
+        <button onClick={handleClick} className="button" value=")">
+          )
+        </button>
+        <button onClick={handleClick} className="button" value="/">
+          ÷
+        </button>
+        <button onClick={handleClick} className="button" value="7">
+          7
+        </button>
+        <button onClick={handleClick} className="button" value="8">
+          8
+        </button>
+        <button onClick={handleClick} className="button" value="9">
+          9
+        </button>
+        <button onClick={handleClick} className="button" value="*">
+          ×
+        </button>
+        <button onClick={handleClick} className="button" value="4">
+          4
+        </button>
+        <button onClick={handleClick} className="button" value="5">
+          5
+        </button>
+        <button onClick={handleClick} className="button" value="6">
+          6
+        </button>
+        <button onClick={handleClick} className="button" value="-">
+          -
+        </button>
+        <button onClick={handleClick} className="button" value="1">
+          1
+        </button>
+        <button onClick={handleClick} className="button" value="2">
+          2
+        </button>
+        <button onClick={handleClick} className="button" value="3">
+          3
+        </button>
+        <button onClick={handleClick} className="button" value="+">
+          +
+        </button>
+        <button onClick={handleClick} className="button" value="0">
+          0
+        </button>
+        <button onClick={handleClick} className="button" value=".">
+          .
+        </button>
+        <button onClick={handleClick} className="button" value="Ans">
+          Ans
+        </button>
+        <button onClick={handleClick} className="button" value="sin(">
+            sin
+        </button>    
+        <button onClick={handleClick} className="button" value=".">
+          .
+        </button>
+        <button onClick={handleClick} className="button" value="Ans">
+          Ans
+        </button>
+        <button onClick={handleClick} className="button" value="sin(">
+          sin
+        </button>
+        <button onClick={handleClick} className="button" value="cos(">
+          cos
+        </button>
+        <button onClick={handleClick} className="button" value="tan(">
+          tan
+        </button>
+        <button onClick={handleClick} className="button" value="ln(">
+          ln
+        </button>
+        <button onClick={handleClick} className="button" value="log(">
+          log
+        </button>
+        <button onClick={handleClick} className="button" value="e^(">
+          e^x
+        </button>
+        <button onClick={handleSquareRoot} className="button" value="sqrt(">
+          √
+        </button>
+        <button onClick={handlePower} className="button" value="^">
+          x^y
+        </button>
+        <button onClick={handleClick} className="button" value="%">
+          %
+        </button>
+        <button onClick={handleCalculate} className="button equal" value="=">
+          =
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
+export default Calculator;
